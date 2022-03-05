d1c8c9adf49bebd6f6f077b7f1a32274d4852c27
tambarie <gt.gbaragbo@gmail.com>
2022-03-05 17:41:03 +0100

    add SetMovieCharactersInRedis and GetMovieCharactersInRedis method

--- internal/adapters/repository/redisdb/redisService.go
+++ internal/adapters/repository/redisdb/redisService.go
@@ -10,8 +10,6 @@ import (
 )
 
 func (r *RedisCache) SetMovie(key string, value *[]domain.Movie) error {
-	log.Println("setting context")
-	fmt.Println("setting context")
 	client := r.getClient()
 	json, err := json.Marshal(value)
 	if err != nil {
@@ -42,10 +40,33 @@ func (r *RedisCache) GetMovie(key string) *[]domain.Movie {
 	return &movie
 }
 
-func (r *RedisCache) SetMovieCharacters(key string, value []domain.Character) {
-	return
+func (r *RedisCache) SetMovieCharactersInRedis(key string, value []domain.Character) error {
+	client := r.getClient()
+	json, err := json.Marshal(value)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = client.Set(context.Background(), key, string(json), r.expires*time.Minute).Err()
+	if err != nil {
+		return err
+	}
+	pong, err := client.Ping(context.Background()).Result()
+	log.Println(pong, err)
+	return err
 }
 
-func (r *RedisCache) GetMovieCharacters(key string) []domain.Character {
-	return nil
+func (r *RedisCache) GetMovieCharactersInRedis(key string) []domain.Character {
+	redisClient := r.getClient()
+	val, err := redisClient.Get(context.Background(), key).Result()
+	if err != nil {
+		return nil
+	}
+	var character []domain.Character
+	err = json.Unmarshal([]byte(val), &character)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Movies retrieved from cache")
+	return character
 }
