7546439ac86bf201e0265269cf3dec6bf93cb3ca
tambarie <gt.gbaragbo@gmail.com>
2022-03-05 17:33:22 +0100

    remove ID field in comment struct

--- internal/core/domain/movie/comment.go
+++ internal/core/domain/movie/comment.go
@@ -3,8 +3,7 @@ package domain
 type Comment struct {
 	Model
 	tableName struct{} `pg:"movie_comments"`
-	ID        uint     `gorm:"primaryKey;autoIncrement;"`
-	MovieId   int      `json:"movie_id" pg:"movie_id"`
-	IP        string   `json:"ip" pg:"ip"`
+	MovieID   int      `json:"movie_id" pg:"movie_id"`
 	Content   string   `json:"content" pg:"content"`
+	IP        string   `json:"ip" pg:"ip"`
 }
