6098cbcc49bdc9e3d00b51478a4abe92aab64db5
tambarie <gt.gbaragbo@gmail.com>
2022-03-06 18:26:01 +0100

    commented swagger]

--- movie-service.go
+++ movie-service.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/Tambarie/movie-api/docs"
 	api "github.com/Tambarie/movie-api/internal/adapters/api/movie"
 	"github.com/Tambarie/movie-api/internal/adapters/repository/postgresdb"
 	"github.com/Tambarie/movie-api/internal/adapters/repository/redisdb"
@@ -11,9 +10,6 @@ import (
 	"github.com/Tambarie/movie-api/internal/core/shared"
 	"github.com/Tambarie/movie-api/internal/ports"
 	"github.com/gin-gonic/gin"
-	ginSwagger "github.com/swaggo/gin-swagger"
-
-	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"log"
 	"time"
 )
@@ -47,13 +43,13 @@ func main() {
 	router := gin.Default()
 	router.Use(helper.LogRequest)
 
-	docs.SwaggerInfo.BasePath = "/api"
+	//docs.SwaggerInfo.BasePath = "/api"
 	router.GET("/api/movies", handler.GetMovies())
 	router.GET("/api/movies/:movieID/characters", handler.GetMoviesCharacters())
 	router.POST("/api/movies/:movieID/comments", handler.AddCommentToMovies())
 	router.GET("/api/movies/:movieID/comments", handler.GetCommentsInMovie())
 
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	//router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.NoRoute(func(ctx *gin.Context) {
 		ctx.JSON(404,
 			helper.PrintErrorMessage("404", shared.NoResourceFound))
