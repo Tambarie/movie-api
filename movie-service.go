4afb555143aac12b2fb7e2ade1c639b9d9bb7ec2
tambarie <gt.gbaragbo@gmail.com>
2022-03-06 19:35:10 +0100

    add conditions if redis host is present in env

--- movie-service.go
+++ movie-service.go
@@ -31,8 +31,14 @@ import (
 // @securityDefinitions.basic  BasicAuth
 func main() {
 	helper.InitializeLogDir()
+	var Addr string
 	_, postgresdb_pass, postgres_database_url, service_address, service_port, _, postgresdb_host, postgresdb_mode, postgresdb_name, postgresdb_user, postgresdb_port, postgresdb_timezone, redis_host, redis_port, _ := helper.LoadConfig()
-	Addr := fmt.Sprintf("%s:%s", redis_host, redis_port)
+
+	if os.Getenv("redis_host") == "" || os.Getenv("redis_port") == "" {
+		Addr = fmt.Sprintf("%s:%s", redis_host, redis_port)
+	} else {
+		Addr = fmt.Sprintf("%s:%s", os.Getenv("redis_host"), os.Getenv("redis_port"))
+	}
 
 	dbRepository := ConnectToPostgres(postgresdb_user, postgresdb_pass, postgres_database_url, postgresdb_host, postgresdb_name, postgresdb_port, postgresdb_mode, postgresdb_timezone)
 	log.Println(postgresdb_name)
