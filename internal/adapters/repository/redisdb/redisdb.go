e5b8f8b9ee4bd50909ccd2e9a39a381435e46ee7
tambarie <gt.gbaragbo@gmail.com>
2022-03-06 20:00:43 +0100

    modify

--- internal/adapters/repository/redisdb/redisdb.go
+++ internal/adapters/repository/redisdb/redisdb.go
@@ -23,18 +23,20 @@ func NewRedisClient(host string, db int, expiry time.Duration) ports.RedisReposi
 }
 
 func (r *RedisCache) getClient() *redis.Client {
+	var redisURL *redis.Options
 	if os.Getenv("REDIS_URL ") == "" {
-		return redis.NewClient(&redis.Options{
+		redisURL = &redis.Options{
 			Addr:     r.host,
 			Password: "",
 			DB:       r.db,
-		})
+		}
 	} else {
-		redisURL, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+		var err error
+		redisURL, err = redis.ParseURL(os.Getenv("REDIS_URL"))
 		if err != nil {
 			panic(err)
 		}
-		return redis.NewClient(redisURL)
-	}
 
+	}
+	return redis.NewClient(redisURL)
 }
