24b4d0e4754ee43aacdac2a9397ba36f907fe7cf
tambarie <gt.gbaragbo@gmail.com>
2022-03-07 14:19:16 +0100

    add comments to movieService.go

--- internal/ports/movieService.go
+++ internal/ports/movieService.go
@@ -2,12 +2,14 @@ package ports
 
 import domain "github.com/Tambarie/movie-api/internal/core/domain/movie"
 
+// MovieService  interface that connects the Handler
 type MovieService interface {
 	SaveComments(comment *domain.Comment) (*domain.Comment, error)
 	GetComments(movieId int) (*[]domain.Comment, error)
 	CountComments(movieId int) (int64, error)
 }
 
+// RedisService  interface that connects the Handler
 type RedisService interface {
 	SetMovie(key string, value *[]domain.Movie) error
 	GetMovie(key string) *[]domain.Movie
