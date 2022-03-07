3bb0b4425fb533837b1920906e3d264113df6013
tambarie <gt.gbaragbo@gmail.com>
2022-03-07 01:47:14 +0100

    create addComments.go file

---
+++ internal/adapters/api/movie/getComments.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+// @Summary Endpoint Gets a list of comments
+// @Description Endpoint Gets a list of comments for a particular movieID
+// @Produce  json
+// @Param movie_id path int true "Movie ID"
+// @Success 200 {object} []domain.Comment
+// @Router /movies/:movieID/comments/ [get]
+// GetComments method returns all comments for a particular movieID
+func (h *HTTPHandler) GetCommentsInMovie() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		movieIDStr := context.Param("movieID")
+		movieID, err := strconv.Atoi(movieIDStr)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, err)
+			return
+		}
+		movieData, err := h.movieService.GetComments(movieID)
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, err)
+		}
+
+		context.JSON(200, gin.H{
+			"message": "comments retrieved",
+			"data":    movieData,
+		})
+	}
+}
