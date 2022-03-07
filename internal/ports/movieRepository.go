9638b3be440b8c4bc6c92747c886d3ba637e6142
tambarie <gt.gbaragbo@gmail.com>
2022-03-07 14:18:48 +0100

    add comments to movie-Repository.go

--- internal/ports/movieRepository.go
+++ internal/ports/movieRepository.go
@@ -2,12 +2,14 @@ package ports
 
 import domain "github.com/Tambarie/movie-api/internal/core/domain/movie"
 
+// MovieRepository interface that connects the postgreDB
 type MovieRepository interface {
 	SaveComments(comment *domain.Comment) (*domain.Comment, error)
 	GetComments(movieId int) (*[]domain.Comment, error)
 	CountComments(movieId int) (int64, error)
 }
 
+// RedisRepository interface that connects the redisDB
 type RedisRepository interface {
 	SetMovie(key string, value *[]domain.Movie) error
 	GetMovie(key string) *[]domain.Movie
